e328171879b24f98d88c61af99706faa5e5760aa
nrasheva <nadezhda.rasheva96@gmail.com>
2023-11-12 09:59:21 +0200

    Implement get comments

--- handlers/comments.js
+++ handlers/comments.js
@@ -24,4 +24,21 @@ async function createComment(req, res) {
   }
 }
 
-module.exports = { createComment };
+async function getComments(req, res) {
+  const id = req.query.id;
+
+  if (!id) {
+    res.status(400).json({ message: 'id parameter not provided' });
+    return;
+  }
+
+  try {
+    const comments = await Comment.find({ movie: id }).exec();
+
+    res.status(200).json(comments);
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+}
+
+module.exports = { createComment, getComments };
--- router.js
+++ router.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const { authMiddleware } = require('./middleware');
-const { createComment } = require('./handlers/comments');
+const { createComment, getComments } = require('./handlers/comments');
 const { discoverMovies, getMovie } = require('./handlers/movies');
 const { getGenres } = require('./handlers/genres');
 const { login, register } = require('./handlers/authentication');
 
 const router = express.Router();
 
+router.get('/comments', authMiddleware, getComments);
 router.get('/discoverMovies', discoverMovies);
 router.get('/getGenres', getGenres);
 router.get('/getMovie', getMovie);
